de2fbf2ac32e699f471de9f7e024c0f902176109
Mei <61218534+bit704@users.noreply.github.com>
2023-03-21 22:56:22 +0800

    misc: 删除多余文件

--- Adonis_server/adonis/src/main/java/com/reddish/adonis/exception/UserInfoException.java
+++
@@ -1,8 +0,0 @@
-package com.reddish.adonis.exception;
-
-public class UserInfoException extends Exception {
-    public ExceptionCode code;
-    public UserInfoException(ExceptionCode code) {
-        this.code = code;
-    }
-}
